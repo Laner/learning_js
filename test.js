7bb1f62e6b06743541d376905513620249b64a87
laner <runar.dankel@gmail.com>
2012-02-03 11:47:49 +0000

    new project with arrays

--- test.js
+++ test.js
@@ -1,29 +1,47 @@
-<html>
-<head>
-<!-- Internal Javascript Function -->
-<script language="javaScript">
-
 console.log("hva skjer?");
-var ti = 10;
-ti;
-var vokaler = "aeiou";
-var setning = "dette er supert";
-var slutt = '';
-var i;
-for (i = 0; i <= setning.length; i++) {
-    if (vokaler.indexOf(setning.substring(i, i+1))<=-1 && setning.substring(i, i+1)!=" ") {
-        slutt += setning.substring(i, i+1) + "o" + setning.substring(i, i+1);
-    }
-    else {
-        slutt += setning.substring(i, i+1);
-    }
-}
-console.log(slutt);
+// setting up an array
+var myArray = new Array("per", "pal", "espen", "askeladden", "prinsessen");
+console.log("checking nr of items in Array: " + myArray.length);
 
-</script>
-<!-- External Javascript Function -->
-<script language="JavaScript" src="javascript.js" type="text/javascript"/>
-</head>
-<body>
-</body>
-</html>
+// running trhough it
+console.log("What is in the Array:");
+for (i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
+console.log("--------");
+
+// finding a spesific name and printing its place
+console.log("Where is espen? ");
+for (i = 0; i <= myArray.length; i++) {
+    if (myArray[i] == "espen") {
+        console.log(i);
+}
+}
+
+// exchanging espen for backwards verison
+console.log("turn espen arround: ");
+var baklengs = '';
+var i;
+for (i = myArray[2].length; i >= 0; i--) {
+    if (i === myArray[2].length) {
+        var bokstav = myArray[2].substring(i);
+    } else {
+    var bokstav = myArray[2].substring(i, i + 1);
+    }
+    console.log(bokstav);
+    baklengs += bokstav;
+}
+myArray[2] = baklengs;
+console.log("snudd?: " + baklengs);
+
+// running trhough it
+console.log("What is in the Array:");
+for (i = 0; i <= myArray.length; i++) {
+    console.log(myArray[i]);
+}
+
+// list input
+console.log("who to turn next?: ");
+for (i = 0; i <= myArray.length; i++) {
+    console.log("(" + i + ") " + myArray[i]);
+}
