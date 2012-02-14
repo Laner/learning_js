2b8373763938e36d378cf86b1d7eeb07e1a25133
laner <runar.dankel@gmail.com>
2012-02-14 12:46:56 +0000

    "mer filer"

---
+++ dimensions.js
@@ -0,0 +1,13 @@
+// script that outputs areal, cubic or surface
+
+//var numbers = prompt("What are the measurements?");
+var numbers = [2, 3, 4];
+
+function checkLength(x) {
+    var i;
+    while (i <= x[i]) {
+        i++;
+    }
+    return i;
+}
+checkLength(numbers);
