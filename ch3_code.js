3b96bc4ced97ec3a02a723ffdc619d530ed68d1b
laner <runar.dankel@gmail.com>
2012-02-23 13:35:11 +0000

    'lesson files added'

---
+++ ch3_code.js
@@ -0,0 +1,12 @@
+// exercises from the book
+// writing in html form js
+document.write("<h1>A page of javascript</h1>");
+var subhead = "A smaller heading, but still JS";
+
+document.write(subhead);
+document.write("<p>");
+var myIntro = 'Hello! Welcome to my Javascript page <br />';
+var linkTag = '<a href = "http://www.pagesource.com"> Link to site</a>';
+document.write(myIntro);
+document.write(linkTag);
+document.write("</p>");
