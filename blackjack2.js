29f21a313f1a043bec5c6adc705729bdd2c73bad
laner <runar.dankel@gmail.com>
2012-03-01 15:15:43 +0000

    'updated blackjack 2'

--- blackjack2.js
+++ blackjack2.js
@@ -19,25 +19,20 @@ function Card(s, n) {
         }
     };
     this.getSuitName = function() {
-        var suitName = "";
         switch (suit) {
             case 1:
-                suitName = "Diamonds";
-                break;
+                return "Diamonds";
             case 2:
-                suitName = "Clubs";
-                break;
+                return "Clubs";
             case 3:
-                suitName = "Hearts";
-                break;
+                return "Hearts";
             case 4:
-                suitName = "Spades";
-                break;
+                return "Spades";
             default:
-                suitName = "No suit";
+                return "No suit";
         }
     };
-    this.getRank = function() {
+    this.getCardName = function() {
         var rank = "";
         switch (number) {
             case 1:
@@ -54,15 +49,37 @@ function Card(s, n) {
                 break;
             default:
                 rank = number;
+                break;
         }
+        return rank;
     };
 }
 // answer to extra credit 1.
 function Hand() {
-    var card1 = new deal();
-    var card2 = new deal();
+    var cards = [];
     this.score = function() {
-        return card1.getValue() + card2.getValue();
+        var idx = cards.length;
+        var score = 0;
+        while (idx > 0) {
+            score += cards[idx - 1].getValue();
+            idx--;
+        }
+        return score;
+    };
+    this.init = function() {
+        cards[0] = new deal();
+        cards[1] = new deal();
+    };
+    this.hitme = function() {
+        cards[cards.length] = new deal();
+    };
+    this.listCards = function() {
+        var idx = cards.length;
+        console.log("In your hand you have: ");
+        while (idx > 0) {
+            console.log(cards[idx - 1].getCardName() + " of " + cards[idx - 1].getSuitName());
+            idx--;
+        }
     };
 }
 // Make a deal function here.  It should return a new card with a suit
@@ -87,8 +104,23 @@ Hand class right now because it only holds 2 cards. How can we change the
 Hand class to make it flexible so we can continue adding cards? 
 (As in when the player says "hit me")
 */
-var myHand = new Hand(); var yourHand = new Hand();
-
-console.log("I scored a "+myHand.score()+" and you scored a "+ yourHand.score());
-console.log("In your hand you have" + myHand.card1.getRank() + " of " + myHand.card1.getSuitName());
-if(yourHand.score() > myHand.score()) console.log("you win!"); else if(yourHand.score() < myHand.score()) console.log("I win!"); else console.log("We tied!");
+var myHand = new Hand(); 
+//var yourHand = new Hand();
+myHand.init();
+//yourHand.init();
+console.log("score: " + myHand.score());
+console.log("-------------------");
+myHand.hitme();
+console.log(myHand.score());
+myHand.listCards();
+//console.log("I scored a "+myHand.score()+" and you scored a "+ yourHand.score());
+//console.log("In your hand you have" + myHand.card1.getRank() + " of " + myHand.card1.getSuitName());
+/*
+if(yourHand.score() > myHand.score()) {
+    console.log("you win!"); 
+    } else if(yourHand.score() < myHand.score()) {
+        console.log("I win!"); 
+    } else {
+        console.log("We tied!");
+    }
+    */
