e11ef16ef0875991da67180efaf6cd612626b849
Runar Dankel <runar.dankel@gmail.com>
2012-03-07 21:15:15 +0100

    deck getting better

--- blackjack-new.js
+++ blackjack-new.js
@@ -1,6 +1,6 @@
-// what should cointain what?
-// should have a card obj that cointains data, is sorted and is pusched into a 
-//deck array the deck should then be shuffled. 
+/* Make a card object that contains suit and rank
+Add methods to retrive the suit and rank so they are kept privat
+*/
 
 function Card(s,r) {
     var suit = s;
@@ -8,67 +8,89 @@ function Card(s,r) {
     this.getSuit = function() {
         return suit;
     };
-    this.getSuitName = function() {
-        switch (suit) {
-            case 1:
-                return "Diamonds";
-            case 2:
-                return "Clubs";
-            case 3:
-                return "Hearts";
-            case 4:
-                return "Spades";
-            default:
-                return "No suit";
-        }
-    };
     this.getRank = function() {
         return rank;
     };
     this.getCard = function() {
-        return this.getRank() + " of " + this.getSuitName();
+        return this.getRank() + " of " + this.getSuit();
     };
     
 }
+
+// make a number of decks from the predefined arrays
+// that filles an array with cards
+// make deck global
 var deck = [];
-// make a deck that filles an array with cards
-function makeDeck(nrOfDecks,shuffel) {
-    var suit = [1, 2, 3, 4];
+function makeDeck(nrOfDecks) {
+    // arrays to fill the deck from
+    var suit = ["Diamonds", "Clubs", "Hearts", "Spades"];
     var rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-    var i, s, r;
+    // set the length of the arrays
     var suitlength = suit.length;
     var ranklength = rank.length;
-    for (i = 0; i < nrOfDecks; i++) {
-        for (s = 0; s < suitlength; s++) {
-            for (r = 0; r < ranklength; r++) {
+    //fill the deck based on the parameters
+    for (var i = 0; i < nrOfDecks; i++) {
+        for (var s = 0; s < suitlength; s++) {
+            for (var r = 0; r < ranklength; r++) {
                 deck.push(new Card(suit[s], rank[r]));
             }
             
         }
     }
-    if (shuffel == "yes") {
-        var deckSize = deck.length;
-        for (i = 0; i < deckSize; i++) {
-            var rndNr1 = Math.floor(Math.random()*deckSize);
-            var tmp = deck[i];
-            deck[i] = deck[rndNr1];
-            deck[rndNr1] = tmp;
-        }
-    }
+    // shuffles the deck by the built in .sort method
+    deck.sort(function() {return 0.5 - Math.random()});
     return deck;
 }
 
+// test to see if the deck is correct and randomized
 function listDeck(d) {
     var decklength = d.length;
-    var i;
-    for (i = 0; i < decklength; i++) {
+    for (var i = 0; i < decklength; i++) {
         console.log( i + ": " + d[i].getCard());
     }
 }
 
+// make a player object that takes care of the scoring
+// and have a hand to store the cards form the deck
 function player(name) {
     this.name = name;
     this.hand = [];
+    this.score = function() {
+        var handsize = this.hand.length;
+        var score = 0;
+        //first we check for aces
+        var haveace = false;
+        for (var i = 0; i < handsize; i++) {
+            if (this.hand[i].getRank() === "A") {
+                haveace = true;
+                break;
+            }
+        }
+        //loop the hand and add score
+        for (var h = 0; h < handsize; h++) {
+            var rank = this.hand[i].getRank();
+            if ( rank === "A") {
+                score += 11;
+            } else if ( rank === "Q") {
+                score += 10;
+            } else {
+                score += this.hand[i].getRank();
+            }
+        }
+        return score;
+    };
 }
-makeDeck(1,"yes");
-listDeck(deck);
+
+makeDeck(1);
+//listDeck(deck);
+var Runar = new player("runar");
+console.log(deck[0]);
+console.log(Runar.hand.length);
+var test = deck[0];
+Runar.hand.push(test);
+deck.splice(0,1);
+console.log(Runar.hand.length);
+console.log("here: " + Runar.hand[0].getSuit());
+var test = Runar.hand[0];
+console.log(test.getRank());
+console.log(Runar.score());
