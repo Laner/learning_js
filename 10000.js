a98c84175190afa1391b12d3aeaa9449a9518946
laner <runar.dankel@gmail.com>
2012-03-14 11:27:55 +0000

    'make a dice game'

---
+++ 10000.js
@@ -0,0 +1,24 @@
+/*
+en fem'er gir 50 
+poeng og en en'er gir 100 poeng.
+Tre en'ere gir 1000 poeng, 
+tre to'ere gir 200 poeng, 
+tre tre'ere gir 300 poeng, 
+tre fir'ere gir 400 poeng, 
+tre fem'ere gir 500 poeng 
+og tre seks'ere gir 600 poeng. 
+Tre par gir 2000 poeng og fullt hus gir 1500 poeng. 
+Du må ta ut minst en terning for hvert kast. 
+Du må få alle like i et kast.Og flere poeng jo flere like. 
+F.eks. 3 firere i et kast gir 400 poeng. 
+4 firere i et kast gir 800 poeng. 
+5 firere i et kast gir 1600 poeng 5 firere i et kast gir 3200 poeng. 
+Og slår du til med 6 firere i et kast gir det hele 6400 poeng. 
+Enere og femere kan du ta ut enkeltvis, 
+men du må ha minst 3 like av de andre. 
+Du må stoppe i omgangen mens du enda får poeng. Ellers mister du alle poengene den omgangen.
+Det er ihvertfall på denne måten vi spiller 10000.
+*/
+// first roll 6 dices and add them to an array
+// output them and ask wich ones to keep.
+//keep in new array
