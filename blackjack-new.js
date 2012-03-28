12721500196b22db1143771ccbd6037914ea451f
Laner <runar.dankel@gmail.com>
2012-03-28 16:53:04 +0300

    Added code from codecademy. todo: Merge it

--- blackjack-new.js
+++ blackjack-new.js
@@ -1,3 +1,136 @@
+// make a number of decks from the predefined arrays
+// that filles an array with cards
+var deck = [];
+function makeDeck(nrOfDecks) {
+    // arrays to fill the deck from
+    var suit = ["Diamonds", "Clubs", "Hearts", "Spades"];
+    var rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+    // set the length of the arrays
+    var suitlength = suit.length;
+    var ranklength = rank.length;
+    //fill the deck based on the parameters
+    for (var i = 0; i < nrOfDecks; i++) {
+        for (var s = 0; s < suitlength; s++) {
+            for (var r = 0; r < ranklength; r++) {
+                var card = new Card(suit[s], rank[r]);
+                deck.push(card);
+            }
+            
+        }
+    }
+    // shuffles the deck by the built in .sort method
+    deck.sort(function() {return 0.5 - Math.random()});
+    return deck;
+}
+// Card Constructor
+function Card(s, n) {
+    var suit = s;
+    var numb = n;
+    this.getSuit = function() {
+        return suit;
+    };
+    this.getNumber = function () {
+        return numb;
+    };
+    this.getValue = function() {
+        if (numb === "A") {
+            return 11;
+        } else if (numb.typof == "string") {
+            return 10;
+        } else {
+            return numb;
+        }
+    };
+}
+
+function Hand() {
+    var cards = [];
+    cards.push(deck.shift());
+    cards.push(deck.shift());
+    this.getHand = function() {
+        return cards;
+    };
+    this.score = function() {
+        var length = cards.length, handscore = 0, nrOfAces = 0;
+        for (var i = 0; i < length; i++) {
+            handscore += cards[i].getValue();
+            if (cards[i].getValue() === 11) {
+                nrOfAces++;
+            }
+        }
+        while (handscore > 21 && nrOfAces > 0) {
+            handscore -= 10;
+            nrOfAces--;
+        }
+        return handscore;
+    };
+    this.printHand = function () {
+        var length = cards.length, suitName = "", handList = "";
+        for (var i = 0; i < length; i++) {
+            var suit = cards[i].getSuit();
+            switch (suit) {
+                case 1:
+                    suitName = "clubs";
+                    break;
+                case 2:
+                    suitName = "diamonds";
+                    break;
+                case 3:
+                    suitName = "hearts";
+                    break;
+                case 4: 
+                    suitName = "spades";
+                    break;
+                default:
+                    suitName = "No name";
+                    break;
+            }
+            handList += cards[i].getValue() + " of " + suitName + ", ";
+        }
+        return handList;
+    };
+    this.hitMe = function() {
+            cards.push(deal());
+    
+    };
+}
+
+function playAsDealer () {
+    var hnd = new Hand();
+    while (hnd.score() <= 16) {
+        hnd.hitMe();
+    }
+    return hnd;
+}
+function playAsUser(){
+     var playerHand = new Hand();
+     while(confirm("Your hand: \n" + playerHand.printHand() + "\nHit?") ===true) {
+         playerHand.hitMe();
+     }
+     return playerHand;
+ }
+function declareWinner(userHand, dealerHand) {
+    function getScore(hand) {
+        return hand.score() > 21 ? 0 : hand.score();
+    }
+    var userScore   = getScore(userHand);
+    var dealerScore = getScore(dealerHand);
+    if (userScore > dealerScore) { return "You win!"; }
+    else if (userScore < dealerScore) { return "You lose!"; }
+    else return "You tied!";
+}
+function playGame() {
+    var player = playAsUser();
+    var dealer = playAsDealer();
+    var result = declearWinner(player, dealer);
+    console.log("Players has: " +player.printHand());
+    console.log("For the score of: " + player.score());
+    console.log("Dealers has: "+ dealer.printHand());
+    console.log("For the score of: " + dealer.score());
+    console.log("result: " + result);
+}
+playGame();
+//-----------------------------------------------------------------------
 /* Make a card object that contains suit and rank
 Add methods to retrive the suit and rank so they are kept privat
 */
