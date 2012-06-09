42ff8b81b2cb1f27af6673a882512e590e1b1bbb
Runar Dankel <runar.dankel@gmail.com>
2012-06-09 09:24:56 +0200

    small update

--- tmp.js
+++ tmp.js
@@ -13,7 +13,10 @@ first.split("").forEach(
 	function(f, i){
 		rot13_dict[f] = second.split("")[i];
 	});
-
+first.split("").forEach(
+        function(elementOfStartA, index, thisArray) {
+            console.log(thisArray[index] + " same " + elementOfStartA);
+        });
 function rot13_map (letter) {
 	var a = (rot13_dict[letter]) ? rot13_dict[letter] : letter;
 	return a;
@@ -22,3 +25,31 @@ function rot13 (arg) {
 	return arg.split("").map(rot13_map).join("");
 }
 console.log(rot13("N pnegbba"));
+
+// function addNewExercise (one, two) {
+	
+// }
+// function processItem () {
+	
+// }
+// function deleteLesson (one, two) {
+    
+// }
+// function encryptFile (one, two) {
+    
+// }
+// function closeDocument (one, two,three) {
+    
+// }
+// function openDocument (one, two) {
+	
+// }
+// openDocument(1,2);
+
+// function openDocument () {
+// 	return 1;
+// }
+// function downloadFile () {
+// 	return 6;
+// }
+// console.log(downloadFile());
