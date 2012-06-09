883d728d5a88c612f054dbc532d062f205b6f9c6
Runar Dankel <runar.dankel@gmail.com>
2012-06-09 09:29:08 +0200

    trying updates

--- Areal.js
+++ Areal.js
@@ -1,16 +1,29 @@
 // script that outputs areal, cubic or surface
 
-console.log(numbers.length);
-checkLength(numbers);
 
-function checkLength(numbers) {
-    if (typeof numbers[0] != 'number') {
+//numbers = new Array(Number(prompt("What are the measurements?")));
+var numbers = prompt("inn");
+var a = new Array([]);
+
+a = Number(numbers.split(","));
+
+console.log(a);
+
+//var numbers = [2, 4];
+//console.log(numbers.length);
+checkLength(a);
+
+function checkLength(x) {
+    if (typeof x[0] != 'number') {
         alert("Input numbers please");
     } else if (numbers.length === 0 || numbers.length ===1) {
+    } else if (x.length === 0 || x.length ===1) {
         alert("Need 2 numbers or more");
     } else if (numbers.length === 2) {
+    } else if (x.length === 2) {
         return "Areal: " + calAreal(numbers);
     } else if (numbers.length === 3) {
+    } else if (x.length === 3) {
         return "Cubic is: " + calCubic(numbers);
     } else {
         return "No can do :(";
@@ -34,3 +47,4 @@ function calCubic(x) {
     }
     return cubic;
 }
+// S.split(/[^0-9\.]/)
