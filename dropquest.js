d94aea889b6fb9f3489337fc4e8aaa7f7898ba8c
Runar Dankel <runar.dankel@gmail.com>
2012-08-09 21:46:29 +0200

    Merge with local copy

--- dropquest.js
+++ dropquest.js
@@ -7,16 +7,13 @@ Function 2 - Replace each vowel with the one before it (a→u, e→a, i→e, o�
 Function 5 - Reverse every word that starts with a letter in the second half of the alphabet.
 Function 9 - Remove all words that have 3 letters or less.*
 */
-function function1(str) {
 function func1(str) {
     var newstr = str.replace(/[DROPBOX]/g,"");
     return newstr;
 }
-var message2 = function1(message);
 var message2 = func1(message);
 console.log(message2 + " func1 -DROPBOX");
 
-function function2(str) {
 function func2(str) {
     var newString = str.replace(/A/gi,"U").replace(/E/gi,"A").replace(/I/gi,"E").replace(/O/gi,"I").replace(/U/gi,"O");
     return newString;
@@ -26,7 +23,7 @@ message2 = function2(message2);
 message2 = func2(message2);
 console.log(message2 + " func2 exchange");
 
-function function5(str) {
+function func5(str) {
     var strArray = str.split(" ");
     for (var i = 0; i < strArray.length; i++) {
         var word = strArray[i];
@@ -37,11 +34,9 @@ function function5(str) {
     return strArray.join(" ");
 }
 
-message2 = function5(message2);
 message2 = func5(message2);
 console.log(message2 + " func5 reverse");
 
-function function9(str) {
 function func9(str) {
     var strArr = str.split(" ");
         for (var i = 0; i < strArr.length; i++) {
@@ -51,6 +46,5 @@ function func9(str) {
         }
     return strArr.join(" ");
 }
-message2 = function9(message2);
 message2 = func9(message2);
 console.log(message2 + " func9 remove");
